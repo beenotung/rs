8f047e0348fbd871a3fddcae98ff0634fd1eaaea
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-02-19 16:44:33 +0800

    added kv demo in elixir

--- doc/learning.md
+++ doc/learning.md
@@ -1,5 +1,5 @@
-##### Using C from Elixir with NIFs
-http://andrealeopardi.com/posts/using-c-from-elixir-with-nifs/
-
 ##### Introduction to Mix (and Elixir)
 http://elixir-lang.org/getting-started/mix-otp/introduction-to-mix.html
+
+##### Using C from Elixir with NIFs
+http://andrealeopardi.com/posts/using-c-from-elixir-with-nifs/
---
+++ elixir/kv/lib/kv/bucket.ex
@@ -0,0 +1,29 @@
+defmodule KV.Bucket do
+  @doc """
+    Start a new bucket.
+  """
+  def start_link do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+    Get the value from the `bucket` by `key`.
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+    Put the `value` into the `bucket` by the `key`.
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  @doc """
+    Delete the `key` from `bucket`, and return the old value.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+end
