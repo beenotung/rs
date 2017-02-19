5639113ca05ee49c51d3e69c1a4f01f971c385ea
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-02-19 13:08:02 +0800

    added empty project in elixir and c

---
+++ elixir/rs/lib/rs.ex
@@ -0,0 +1,18 @@
+defmodule Rs do
+  @moduledoc """
+  Documentation for Rs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rs.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/rs/test/rs_test.exs
@@ -0,0 +1,8 @@
+defmodule RsTest do
+  use ExUnit.Case
+  doctest Rs
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
