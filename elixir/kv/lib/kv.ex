3e58b6268e63283d49b0a9b826041d24bee55364
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-02-20 07:57:53 +0800

    tmp save: adding KV.Bucket.Supervisor

--- elixir/kv/lib/kv.ex
+++ elixir/kv/lib/kv.ex
@@ -1,7 +1,16 @@
 defmodule KV do
+  use Application
+
   @moduledoc """
   Documentation for KV.
   """
+
+  @doc """
+    Start the application kv.
+  """
+  def start(_type, _args) do
+    KV.Supervisor.start_link
+  end
 
   @doc """
   Hello world.
